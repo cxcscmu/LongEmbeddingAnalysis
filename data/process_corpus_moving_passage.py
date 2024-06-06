4b509a3b721813e54e73a76f5495dfb4a362505c
JMVCoelho <joao.vares.coelho@tecnico.ulisboa.pt>
2024-06-06 03:02:13 +0100

    remove hard coded data processing step

--- data/process_corpus_moving_passage.py
+++ data/process_corpus_moving_passage.py
@@ -10,17 +10,18 @@ dev_pass_qrels_path = "./data/marco_passage/qrels.dev.tsv"
 dev_pass_q_path = "./data/marco_passage/dev.query.txt"
 
 MAX_SEQ_LEN = 2048
+NUM_DATA_POINTS = 10 
 
 tokenizer = AutoTokenizer.from_pretrained("jmvcoelho/t5-base-marco-2048") # experiments are model-specific, will need to change this
 
 def create_strings_10(passage, doc):
     # String C
     assert passage in doc # make sure we can exact match the passage in the document!
-    if len(tokenizer.encode(doc)) <= MAX_SEQ_LEN:
+    if len(tokenizer.encode(doc)) <= MAX_SEQ_LEN: # if doc has less than 2048 just move it around
 
         without_passage = doc.replace(passage, '', 1)
 
-        num_intervals = 8
+        num_intervals = NUM_DATA_POINTS-2
         interval_length = len(without_passage) // (num_intervals + 1)
 
         # List to store all generated strings
@@ -41,7 +42,7 @@ def create_strings_10(passage, doc):
         documents.append(document_end)
 
         return documents
-    else:
+    else: # if doc has more than 2048, remove passage, truncate doc to doc-passage, then move passahe around
         without_passage = doc.replace(passage, '', 1)
 
         doc_tok = tokenizer.encode(without_passage)
@@ -138,47 +139,25 @@ with open("./data/qrels.dev.tsv.moving.passage") as h:
         qid, _, did, _ = line.strip().split("\t")
         did2qid[did] = qid
         
-corpus_0 = "./data/moving_passage_experiment/corpus_0.tsv"
-corpus_1 = "./data/moving_passage_experiment/corpus_1.tsv"
-corpus_2 = "./data/moving_passage_experiment/corpus_2.tsv"
-corpus_3 = "./data/moving_passage_experiment/corpus_3.tsv"
-corpus_4 = "./data/moving_passage_experiment/corpus_4.tsv"
-corpus_5 = "./data/moving_passage_experiment/corpus_5.tsv"
-corpus_6 = "./data/moving_passage_experiment/corpus_6.tsv"
-corpus_7 = "./data/moving_passage_experiment/corpus_7.tsv"
-corpus_8 = "./data/moving_passage_experiment/corpus_8.tsv"
-corpus_9 = "./data/moving_passage_experiment/corpus_9.tsv"
+
 
 did2passage_small = {}
 for k in did2passage:
     if did2passage[k] in d_corpus[k]:
         did2passage_small[k] = did2passage[k]
 
-with open(doc_corpus, 'r') as h, \
-    open(corpus_0, 'w') as out1, \
-    open(corpus_1, 'w') as out2, \
-    open(corpus_2, 'w') as out3, \
-    open(corpus_3, 'w') as out4, \
-    open(corpus_4, 'w') as out5, \
-    open(corpus_5, 'w') as out6, \
-    open(corpus_6, 'w') as out7, \
-    open(corpus_7, 'w') as out8, \
-    open(corpus_8, 'w') as out9, \
-    open(corpus_9, 'w') as out10:
+corpus_files = ["./data/moving_passage_experiment/corpus_{}.tsv".format(i) for i in range(NUM_DATA_POINTS)]
 
-    outs = [out1, out2, out3, out4, out5, out6, out7, out8, out9, out10]
-    
-    for line in tqdm(h):
-        did, title, text = line.strip().split("\t")
-    
-        if did in did2qid:
-            
-            docs = create_strings_10(did2passage_small[did].lower(), text.lower())
+with open(doc_corpus, 'r') as h :    
+        for line in tqdm(h):
+            did, title, text = line.strip().split("\t")
+        
+            if did in did2qid:
+                
+                docs = create_strings_10(did2passage_small[did].lower(), text.lower())
 
-            for i in range(len(docs)):
-                title = title.replace("\"", "").replace("\'", "").replace("\n", "").replace("\t", "").replace("\r", "")
-                text = docs[i].replace("\"", "").replace("\'", "").replace("\n", "").replace("\t", "").replace("\r", "")
-                outs[i].write(f"{did}\t{title}\t{text}\n")
-       
-            
-
+                for i in range(len(docs)):
+                    with open(corpus_files[i], 'a') as out:
+                        title = title.replace("\"", "").replace("\'", "").replace("\n", "").replace("\t", "").replace("\r", "")
+                        text = docs[i].replace("\"", "").replace("\'", "").replace("\n", "").replace("\t", "").replace("\r", "")
+                        out.write(f"{did}\t{title}\t{text}\n")
